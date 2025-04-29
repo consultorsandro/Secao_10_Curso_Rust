330ae790cdc1fbf9dba97eae66db956a9622238b
Sandro Reis <consultorsandro@hotmail.com>
2025-04-29 09:23:17 -0300

    Class 74

--- Secao_4_Curso_Rust/src/main.rs
+++ Secao_4_Curso_Rust/src/main.rs
@@ -1,21 +1,39 @@
 fn main() {
-    // Aula 72
-    open_store();
-    bake_pizza();
-    swing_in_profit();
-    swing_in_profit();
+    //Class 74
+    let result = square(5);
+    println!(" The square of 5 is {result}");
 
+}
+    //Function declaration outside of main
+    fn square(number: i32) -> i32 {
+     return number * number;
+    }
+
+
+
+
+
+/*
+    // Aula 72 e 73
+    open_store("Brookling");
+    bake_pizza(20, "pepperoni");
+    swing_in_profit();
+    swing_in_profit();
+    open_store("Queens");
+    bake_pizza(15, "Mussarela");
 
 }
 
-fn open_store() {
+fn open_store(neighborhood: &str) {
+    let _ = neighborhood;
     println!("Opening my pizza store");
 }
 
-fn bake_pizza() {
-    println!("Baking a pizza"); 
+fn bake_pizza(number: i32, topping: &str) {
+    println!("Baking {number} {topping} pizzas"); 
 }
 
 fn swing_in_profit() {
     println!("So much $$$, so little time");
 }
+*/
