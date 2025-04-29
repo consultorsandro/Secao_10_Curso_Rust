debf5c3fae19670537f8598d4a848280603e0213
Sandro Reis <consultorsandro@hotmail.com>
2025-04-29 08:08:43 -0300

    Aula 72 Atualizou git ignore

---
+++ Secao_4_Curso_Rust/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // Aula 72
+    open_store();
+    bake_pizza();
+    swing_in_profit();
+    swing_in_profit();
+
+
+}
+
+fn open_store() {
+    println!("Opening my pizza store");
+}
+
+fn bake_pizza() {
+    println!("Baking a pizza"); 
+}
+
+fn swing_in_profit() {
+    println!("So much $$$, so little time");
+}
