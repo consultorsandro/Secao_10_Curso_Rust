731b9578ef432ba4a0041bc0e7f9e38925faf36f
Sandro Reis <consultorsandro@hotmail.com>
2025-04-29 15:42:24 -0300

    Class 78

--- Secao_4_Curso_Rust/src/main.rs
+++ Secao_4_Curso_Rust/src/main.rs
@@ -1,4 +1,30 @@
+fn apply_to_jobs(number: i32, title: &str) {
+    println!("Applying to {number} {title} jobs");
+}   
+
+fn is_even(number: i32) -> bool {
+    number % 2 == 0
+}
+
+fn alphabets(text: &str) -> (bool, bool) {
+    (text.contains("a"), text.contains("b"))
+}
+
 fn main() {
+    // Class 78
+    apply_to_jobs(35, "Rust Developer");
+
+    println!("{}", is_even(4));
+    println!("{}", is_even(5)); 
+
+    println!("{:?}", alphabets("aardvark"));
+    println!("{:?}", alphabets("zoology"));
+    println!("{:?}", alphabets("zebra"));
+
+
+}
+
+/*
     //Class 77
     let multiplier = 3;
 
@@ -8,8 +34,7 @@ fn main() {
     };
 
     println!("The result of the calculation is {calculation}");
-
-}
+*/
     /*
        //Class 76
     let result = mystery();
