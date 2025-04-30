d7c4f2bbd3cfb727392a0a525f836a0d32f1947e
Sandro Reis <consultorsandro@hotmail.com>
2025-04-30 09:21:23 -0300

    Class 83

--- Secao_4_Curso_Rust/src/main.rs
+++ Secao_4_Curso_Rust/src/main.rs
@@ -1,6 +1,6 @@
-fn apply_to_jobs(number: i32, title: &str) {
+/* fn apply_to_jobs(number: i32, title: &str) {
     println!("Applying to {number} {title} jobs");
-}   
+}
 
 fn is_even(number: i32) -> bool {
     number % 2 == 0
@@ -9,20 +9,33 @@ fn is_even(number: i32) -> bool {
 fn alphabets(text: &str) -> (bool, bool) {
     (text.contains("a"), text.contains("b"))
 }
-
+*/
 fn main() {
+    //Class 83
+    let season = "summer";
+    if season == "summer" {
+        println!("Scools out!");
+    } else if season == "winter" {
+        println!("Brrr, it's cold!");
+    } else if season == "fall" {
+        println!("Leaves are falling!");
+    } else if season == "spring" {
+        println!("Lots of rain!");
+    } else {
+        println!("I don't know that season");
+    }
+}
+/*
     // Class 78
     apply_to_jobs(35, "Rust Developer");
 
     println!("{}", is_even(4));
-    println!("{}", is_even(5)); 
+    println!("{}", is_even(5));
 
     println!("{:?}", alphabets("aardvark"));
     println!("{:?}", alphabets("zoology"));
     println!("{:?}", alphabets("zebra"));
-
-
-}
+*/
 
 /*
     //Class 77
@@ -35,7 +48,7 @@ fn main() {
 
     println!("The result of the calculation is {calculation}");
 */
-    /*
+/*
        //Class 76
     let result = mystery();
 
@@ -53,7 +66,7 @@ fn main() {
     let result = square(13);
     println!(" The square of 13 is {result}");
 
-    
+
 
 }
     //Function declaration outside of main
@@ -62,8 +75,6 @@ fn main() {
     }
 
 */
-
-
 
 /*
     // Aula 72 e 73
@@ -82,7 +93,7 @@ fn open_store(neighborhood: &str) {
 }
 
 fn bake_pizza(number: i32, topping: &str) {
-    println!("Baking {number} {topping} pizzas"); 
+    println!("Baking {number} {topping} pizzas");
 }
 
 fn swing_in_profit() {
