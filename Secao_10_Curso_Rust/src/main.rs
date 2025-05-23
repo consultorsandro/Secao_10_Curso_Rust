7f3fd52e57964278273a8093a3c2ed54204d0812
Sandro Reis <consultorsandro@hotmail.com>
2025-05-23 08:33:42 -0300

    Class 168 e 169

--- Secao_10_Curso_Rust/src/main.rs
+++ Secao_10_Curso_Rust/src/main.rs
@@ -1,3 +1,22 @@
+#[derive(Debug)] // Class 168 e 169
+
+enum PaymentMethodType {
+    Credicard(String), 
+    Debitcard(String),
+    PayPal(String, String),
+}
+
+
+fn main() {
+    let mut my_payment_Method = PaymentMethodType::Credicard(String::from("0034 5678 9012 3456"));
+
+    my_payment_Method = 
+    PaymentMethodType::PayPal(String::from("bob@email.com"), String::from("password"));
+
+    println!("{:?}", my_payment_Method);
+}
+
+/*
 #[derive(Debug)]
 
 enum Cardsuit { // Class 167
@@ -5,19 +24,16 @@ enum Cardsuit { // Class 167
     Hearts,
     Diamonds,
     Clubs,
-    
-}
-
-struct Card {
+    }
+    struct Card {
     rank: String,
     suit: Cardsuit,
-}
-fn main() {
+    }
+ fn main
     let first_card = Cardsuit::Hearts;
     let mut second_card = Cardsuit::Spades;
     second_card = Cardsuit::Clubs;
     println!("{:?}", second_card);
 
     let card_suits = [Cardsuit::Spades, Cardsuit::Hearts];
-    let card_suits = [Cardsuit::Spades, Cardsuit::Diamonds];
-}
+    let card_suits = [Cardsuit::Spades, Cardsuit::Diamonds];*/
